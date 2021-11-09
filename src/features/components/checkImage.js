1a7b0eefb42036a59111788238536ce6a5deca78
Omar Sayed <orhman@hotmail.com>
2021-11-10 00:36:09 +0200

    fetch instead of xhtml

--- src/features/components/checkImage.js
+++ src/features/components/checkImage.js
@@ -1,13 +1,18 @@
-export default function imageExists(image_url) {
-    try {
-        var http = new XMLHttpRequest();
-
-        http.open("HEAD", image_url, false);
-        http.send();
-
-        return http.status != 404;
-    } catch (error) {
-       return false; 
-    }
+export default  function imageExists(image_url) {
+  fetch(image_url, {
+    method: "HEAD",
+    mode: "no-cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch((err) => false);
   
 }
